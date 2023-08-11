9a1b553dd970026523fe01822f802de767db5c0d
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-08-11 16:38:56 -0300

    add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <div>
+      <Header />
+      <div className="h-screen">
         <img src="/padel-icon-1.png"></img>
         <h1 className="text-blue-300">Puro Padel</h1>
       </div>
+      <Footer />
     </>
   );
 }
